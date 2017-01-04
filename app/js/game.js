c599af7d8557ba0c2bca4bacb219297c2e913d9c
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-01-03 20:29:30 -0500

    ronald walking animation works--AGAIN

--- app/js/game.js
+++ app/js/game.js
@@ -58,7 +58,6 @@ Crafty.c('Actor', {
 Crafty.c('Bushes', {
   init: function() {
     this.requires('Actor, spr_bush, Solid')
-    //   .color('rgb(20, 185, 40)')
   }
 });
 
@@ -66,7 +65,6 @@ Crafty.c('Bushes', {
 Crafty.c('Trees', {
   init: function() {
     this.requires('Actor, spr_tree, Solid');
-    // this.color('rgb(20, 125, 40)');
   },
 });
 
@@ -81,36 +79,49 @@ Crafty.c('Player1', {
       .bind()
       .multiway(100,{
           W: -90, S: 90, D: 0, A: 180
-	  });
+	  })
 	  // These next lines define our four animations
 	 //  each call to .animate specifies:
 	 //  - the name of the animation
 	 //  - the x and y coordinates within the sprite
 	 //     map at which the animation set begins
 	 //  - the number of animation frames *in addition to* the first one
-	//  .reel('PlayerMovingUp',    32, 988, 0, 2)
-	//  .reel('PlayerMovingRight', 32, 722, 0, 2)
-	//  .reel('PlayerMovingDown',  32, 152, 2)
-	//  .reel('PlayerMovingLeft',  0, 494, 0, 2);
+	 .reel('RonaldMovingUp', 1000, [
+		 [32, 988], [0, 1026], [32,1026], [0,1064],
+		 [32, 1064], [0,1102], [32, 1102], [0,1140],
+		 [32,1140], [0,1178], [32,1178], [0,1216],
+		 [32,1216]
+	 ])
+	 .reel('RonaldMovingRight', 1000, [
+		 [32, 722], [0,760], [32,760], [0,798],
+		 [32,798], [0,836], [32,836], [0,874]
+	 ])
+	 .reel('RonaldMovingDown', 1000, [
+		 [32,152], [0,190], [32,190], [0,228],
+		 [32,228], [0,266], [32,266], [0,304],
+		 [32,304], [0,342], [32,342], [0,380]
+	 ])
+	 .reel('RonaldMovingLeft', 1000, [
+		 [0, 494], [32,494], [0,532], [32,532],
+		 [0,570], [32,570], [0,608], [32,608]
+	 ]);
 
 	// Watch for a change of direction and switch animations accordingly
-	// var animation_speed = 8;
-	// this.bind('NewDirection', function(data) {
-	//  if (data.x > 0) {
-	//    this.animate('PlayerMovingRight', animation_speed, -1);
-	//  } else if (data.x < 0) {
-	//    this.animate('PlayerMovingLeft', animation_speed, -1);
-	//  } else if (data.y > 0) {
-	//    this.animate('PlayerMovingDown', animation_speed, -1);
-	//  } else if (data.y < 0) {
-	//    this.animate('PlayerMovingUp', animation_speed, -1);
-	//  } else {
-	//    this.stop();
-	//  }
-	// });
+	var animation_speed = 8;
+	this.bind('NewDirection', function(data) {
+		 if (data.x > 0) {
+		   this.animate('RonaldMovingRight', animation_speed, -1);
+		 } else if (data.x < 0) {
+		   this.animate('RonaldMovingLeft', animation_speed, -1);
+		 } else if (data.y > 0) {
+		   this.animate('RonaldMovingDown', animation_speed, -1);
+		 } else if (data.y < 0) {
+		   this.animate('RonaldMovingUp', animation_speed, -1);
+		 } else {
+		   this.pauseAnimation();
+		 }
+	});
 	}
-
-	//   .reel("ronald_right_walk", 12,0,0,8);
 
 });
 
@@ -167,5 +178,3 @@ Crafty.scene("Main", function () {
 	      }
     	}
 });
-
-// animations
