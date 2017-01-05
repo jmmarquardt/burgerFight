b731c9161ab11462965b08400c1b3fe40569ae56
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-01-05 00:26:08 -0500

    burger king and ronald animation references in game.js, sprites unpacked working on king sprites

--- app/js/assetObj.js
+++ app/js/assetObj.js
@@ -1,14 +1,17 @@
-// sprite sheet arrays
+// sprite sheet arrays--private variables to this file.
+// When sprite sheet changes, just update the below frame numbers from
+// spritesheet.json
 var spritesheet = require('../../public/assets/img/sprites/spritesheet.json'),
-  frame = spritesheet.frames,
   bushSprite = spritesheet.frames[2],
   ronaldSprite = spritesheet.frames[3],
-  kingSprite = spritesheet.frames[2],
-  treeSprite = spritesheet.frames[-1];
-
+  kingSprite = spritesheet.frames[1],
+  treeSprite = spritesheet.frames[67],
+  groundSprite = spritesheet.frames[0];
+// ============================================================================
+// exporting assetObj
 module.exports = {
   Ronald: {
-  	move: {
+  	walk: {
   		up: [
   			[32, 988], [0, 1026], [32,1026], [0,1064],
   			[32, 1064], [0,1102], [32, 1102], [0,1140],
@@ -29,27 +32,50 @@ module.exports = {
   			[0,570], [32,570], [0,608], [32,608]
   		]
   	},
+    throw: {
+      up: [],
+      down: [],
+      left: [],
+      right: []
+    }
   },
   // the burger King
   King: {
-
+  	walk: {
+  		up: [
+  			[32, 988], [0, 1026], [32,1026], [0,1064],
+  			[32, 1064], [0,1102], [32, 1102], [0,1140],
+  			[32,1140], [0,1178], [32,1178], [0,1216],
+  			[32,1216]
+  		],
+  		down: [
+  			[32,152], [0,190], [32,190], [0,228],
+  			[32,228], [0,266], [32,266], [0,304],
+  			[32,304], [0,342], [32,342], [0,380]
+  		],
+  		right: [
+  			[32, 722], [0,760], [32,760], [0,798],
+  			[32,798], [0,836], [32,836], [0,874]
+  		],
+  		left: [
+  			[0, 494], [32,494], [0,532], [32,532],
+  			[0,570], [32,570], [0,608], [32,608]
+  		]
+  	},
+    throw: {
+      up: [],
+      down: [],
+      left: [],
+      right: []
+    }
   },
   // our static sprites
   sprites: {
-      // ronaldSprite: frame[3],
-      // x: frame['x'],
-      // y: frame.y,
-      // w: frame.w,
-      // h: frame.h,
-  	  // spr_ronald: [frame[3].x, frame[3].y, frame[3].w, frame[3].h],
-  	  // spr_king: [frame[1].x, frame[1].y, frame[1].w, frame[1].h],
-  	  // spr_bush: [frame[2].x, frame[2].y, frame[2].w, frame[2].h],
-      spr_ronald: [0,38,32,38],
-      spr_king: [12,0,32,38],
+      spr_ronald: [ronaldSprite.frame.x, ronaldSprite.frame.y, ronaldSprite.frame.w, ronaldSprite.frame.h],
+      spr_king: [kingSprite.frame.x, kingSprite.frame.y, kingSprite.frame.w, kingSprite.frame.h],
       // spr_bush: [48,0,16,16],
-      spr_bush: [bushes.frame.x, bushes.frame.y, bushes.frame.w, bushes.frame.h],
-  	  spr_tree: [0, 1254, 16, 16]
+      spr_bush: [bushSprite.frame.x, bushSprite.frame.y, bushSprite.frame.w, bushSprite.frame.h],
+  	  spr_tree: [treeSprite.frame.x, treeSprite.frame.y, treeSprite.frame.w, treeSprite.frame.h],
+      spr_ground: [groundSprite.frame.x, groundSprite.frame.y, groundSprite.frame.w, groundSprite.frame.h]
   }
 };
-
-// module.exports = Ronald;
--- app/js/game.js
+++ app/js/game.js
@@ -1,29 +1,29 @@
-var assets = require('./assetObj.js');
-var Ronald = assets.Ronald;
-var Sprites = assets.sprites;
+var assets = require('./assetObj.js'),
+	Ronald = assets.Ronald,
+	King = assets.King,
+	Sprites = assets.sprites;
+// ============================================================================
 
 exports.Game = {
 	start: function() {
-	    Crafty.init(this.width(), this.height(), document.getElementById("game"));
-	    // Crafty.background('rgb(249, 223, 125)');
-		// Crafty.background('rgb(142, 104, 4)');
+    Crafty.init(this.width(), this.height(), document.getElementById("game"));
 		Crafty.background('url(/assets/img/sprites/ground-2.png)')
 	 	Crafty.scene("Loading");
-    },
-    map_grid: {
-	    width:  38,
-	    height: 38,
-	    tile: {
-	      width:  16,
-	      height: 16
-	    }
+  },
+  map_grid: {
+    width:  38,
+    height: 38,
+    tile: {
+      width:  16,
+      height: 16
+    }
 	},
 	width: function() {
 	    return this.map_grid.width * this.map_grid.tile.width;
 	},
-  	height: function() {
-    	return this.map_grid.height * this.map_grid.tile.height;
-  	},
+	height: function() {
+  	return this.map_grid.height * this.map_grid.tile.height;
+	},
 };
 
 // game grid
@@ -84,46 +84,82 @@ Crafty.c('Player1', {
       .multiway(100,{
           W: -90, S: 90, D: 0, A: 180
 	  })
-	 // These next lines define our four animations
+	 // These next lines define our animations
 	 //  each call to .animate specifies:
 	 //  - the name of the animation
 	 //  - the array coordinates within the sprite
 	 //  map at which the animation set
-	 .reel('RonaldMovingUp', 600, Ronald.move.up)
-	 .reel('RonaldMovingRight', 600, Ronald.move.right)
-	 .reel('RonaldMovingDown', 600, Ronald.move.down)
-	 .reel('RonaldMovingLeft', 600, Ronald.move.left);
+	 .reel('RonaldWalkUp', 500, Ronald.walk.up)
+	 .reel('RonaldWalkRight', 500, Ronald.walk.right)
+	 .reel('RonaldWalkDown', 500, Ronald.walk.down)
+	 .reel('RonaldWalkLeft', 500, Ronald.walk.left)
+	 .reel('RonaldThrowUp', 500, Ronald.throw.up)
+	 .reel('RonaldThrowRight', 500, Ronald.throw.right)
+	 .reel('RonaldThrowDown', 500, Ronald.throw.down)
+	 .reel('RonaldThrowLeft', 500, Ronald.throw.left);
 
 	// Watch for a change of direction and switch animations accordingly
-	var animation_speed = 8;
-	this.bind('NewDirection', function(data) {
-		 if (data.x > 0) {
-		   this.animate('RonaldMovingRight', animation_speed, -1);
-		 } else if (data.x < 0) {
-		   this.animate('RonaldMovingLeft', animation_speed, -1);
-		 } else if (data.y > 0) {
-		   this.animate('RonaldMovingDown', animation_speed, -1);
-		 } else if (data.y < 0) {
-		   this.animate('RonaldMovingUp', animation_speed, -1);
-		 } else {
-		   this.pauseAnimation();
-		 }
-	});
+		var animation_speed = 8;
+		this.bind('NewDirection', function(data) {
+			 if (data.x > 0) {
+			   this.animate('RonaldWalkRight', animation_speed, -1);
+			 } else if (data.x < 0) {
+			   this.animate('RonaldWalkLeft', animation_speed, -1);
+			 } else if (data.y > 0) {
+			   this.animate('RonaldWalkDown', animation_speed, -1);
+			 } else if (data.y < 0) {
+			   this.animate('RonaldWalkUp', animation_speed, -1);
+			 } else {
+			   this.pauseAnimation();
+			 }
+		});
 	}
-
 });
 
 // player 2
 Crafty.c('Player2', {
   init: function() {
     this.requires('Actor, Solid, Multiway, spr_king, Collision, SpriteAnimation')
-      .bind('Moved', function(evt){
-        if (this.hit('Solid'))
-          this[evt.axis] = evt.oldValue;
-      })
+    .bind('Moved', function (evt) {
+      if (this.hit('Solid'))
+        this[evt.axis] = evt.oldValue;
+    })
 	  .bind()
-      .multiway(100,{
-      	UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
+    .multiway(100,{
+			UP_ARROW: -90,
+			DOWN_ARROW: 90,
+			RIGHT_ARROW: 0,
+			LEFT_ARROW: 180
+		})
+		// These next lines define our animations
+		//  each call to .animate specifies:
+		//  - the name of the animation
+		//  - the array coordinates within the sprite
+		//  map at which the animation set
+		.reel('KingWalkUp', 500, King.walk.up)
+		.reel('KingWalkRight', 500, King.walk.right)
+		.reel('KingWalkDown', 500, King.walk.down)
+		.reel('KingWalkLeft', 500, King.walk.left)
+		.reel('KingThrowUp', 500, King.throw.up)
+		.reel('KingThrowRight', 500, King.throw.right)
+		.reel('KingThrowDown', 500, King.throw.down)
+		.reel('KingThrowLeft', 500, King.throw.left);
+
+		// Watch for a change of direction and switch animations accordingly
+		var animation_speed = 8;
+		this.bind('NewDirection', function(data) {
+			if (data.x > 0) {
+				this.animate('KingWalkRight', animation_speed, -1);
+			} else if (data.x < 0) {
+				this.animate('KingWalkLeft', animation_speed, -1);
+			} else if (data.y > 0) {
+				this.animate('KingWalkDown', animation_speed, -1);
+			} else if (data.y < 0) {
+				this.animate('KingWalkUp', animation_speed, -1);
+			} else {
+				this.pauseAnimation();
+			}
+		});
   }
 });
 
@@ -149,15 +185,15 @@ Crafty.scene("Main", function () {
     Crafty.e('Player2').at(15, 5);
 
 		for (var x = 0; x < exports.Game.map_grid.width; x++) {
-	      for (var y = 0; y < exports.Game.map_grid.height; y++) {
-			var at_edge = x === 0 || x === exports.Game.map_grid.width - 1 || y === 0 || y === exports.Game.map_grid.height - 1;
-	        if (at_edge) {
-	        	Crafty.e("Trees").at(x, y);
-	        } else if (Math.random() < 0.03) {
-	            Crafty.e("Bushes").at(x, y);
-	        }
-	      }
-    	}
+      for (var y = 0; y < exports.Game.map_grid.height; y++) {
+				var at_edge = x === 0 || x === exports.Game.map_grid.width - 1 || y === 0 || y === exports.Game.map_grid.height - 1;
+        if (at_edge) {
+        	Crafty.e("Trees").at(x, y);
+        } else if (Math.random() < 0.03) {
+          Crafty.e("Bushes").at(x, y);
+        }
+      }
+  	}
 });
 
 // Sprites declared
@@ -165,5 +201,6 @@ Crafty.sprite(1,"/assets/img/sprites/spritesheet.png", {
 	spr_ronald: Sprites.spr_ronald,
 	spr_king: Sprites.spr_king,
 	spr_bush: Sprites.spr_bush,
-	spr_tree: Sprites.spr_tree
+	spr_tree: Sprites.spr_tree,
+	spr_ground: Sprites.spr_ground
 });
