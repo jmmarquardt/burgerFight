9a63ae28323e88294206dbb89417de1e13a60c53
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-01-16 22:06:22 -0500

    stormpath done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express    = require("express"),
     bodyParser = require("body-parser"),
     app        = express(),
-    // stormpath  = require("express-stormpath"),
+    stormpath  = require("express-stormpath"),
     Router     = express.Router();
 
 var PORT = process.env.PORT || 3000;
@@ -11,12 +11,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static("./public"));
-// app.use(stormpath.init(app, {
-  // website: true
-// }));
+// start stormpath and set future routing
+app.use(stormpath.init(app, {
+  application: {
+    href: process.env.STORMPATH_APPLICATION_HREF
+  },
+  website: true,
+  web: {
+    login: {
+      nextUri: '/'
+    }
+  }
+}));
 
-// app.on('stormpath.ready', function() {
+app.on('stormpath.ready', function() {
+  console.log("Stormpath Ready.");
 	app.listen(process.env.PORT || PORT, function () {
 		console.log("Listening on port ", PORT);
 	});
-// });
+});
