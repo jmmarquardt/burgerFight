83bcec4f2c298c167f0ec6f9b1b391e4e730d74c
MickeyR <michael.anthony.ryan@ibm.com>
2017-02-18 19:17:32 -0500

    fix crafty deprecating our functions.........

--- app/js/tween.js
+++ app/js/tween.js
@@ -1,43 +1,43 @@
 exports.getTweenDirection = function (player) {
-
-	var dir = player._activeDirections,
+	
+	var dir = player._direction,
 		fromTop = {x:player.x + 14, y:player.y},
 		fromBottom = {x:player.x + 14, y:player.y + 50};
 	
-		if(dir[0] === 1 && dir[90] === 1) { // down and right
+		if(dir.x === 1 && dir.y === 1) { // down and right
 
 			return [{x: player._x + 550, y: player._y + 550}, fromTop];
 			
-		} else if(dir[90] === 1 && dir[180] === 1) { // down and left
+		} else if(dir.x === -1 && dir.y === 1) { // down and left
 
 			return [{x: player._x - 550, y: player._y + 550}, fromTop];
 			
-		} else if(dir[90] === 1) { // down
+		} else if(dir.y === 1 || player._currentReelId === "RonaldWalkDown" || player._currentReelId === "KingWalkDown") { // down
 			
 			return [{y: player._y + 550}, fromBottom];
 		
-		} else if(dir[180] === 1 && dir[-90] === 1) { // up and left
+		} else if(dir.x === -1 && dir.y === -1) { // up and left
 			
 			return [{x: player._x - 550, y: player._y - 550}, fromTop];
 		
-		} else if(dir[0] === 1 && dir[-90] === 1) { // up and right
+		} else if(dir.x === 1 && dir.y === -1) { // up and right
 
 			return [{x: player._x + 550, y: player._y - 550}, fromTop];
 		
-		} else if(dir[-90] === 1) { // up
+		} else if(dir.y === -1 || player._currentReelId === "RonaldWalkUp" || player._currentReelId === "KingWalkUp") { // up
 			
 			return [{y: player._y - 550}, fromTop];
 		
-		} else if(dir[180] === 1) { // left
+		} else if(dir.x === -1 || player._currentReelId === "RonaldWalkLeft" || player._currentReelId === "KingWalkLeft") { // left
 			
 			return [{x: player._x - 550}, fromTop];
 		
-		} else if(dir[0] === 1) { // right
+		} else if(dir.x === 1 || player._currentReelId === "RonaldWalkRight" || player._currentReelId === "KingWalkRight") { // right
 			
 			return [{x: player._x + 550}, fromTop];
 		
-		} else { // default
-			
-			return [{y: player._y + 550}, fromTop];
+		} else { 
+
+			return [{y: player._y + 550}, fromBottom];
 		}
 }
